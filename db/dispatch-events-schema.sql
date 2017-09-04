869b7440dacb69c19e4d740d2f8be032006f91ad
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-09-04 01:27:50 -0500

    add database... resolve #3

---
+++ db/dispatch-events-schema.sql
@@ -0,0 +1,46 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.19)
+# Database: dispatch
+# Generation Time: 2017-09-04 03:17:14 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table events
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `event_number` char(9) NOT NULL DEFAULT '',
+  `full_address` varchar(64) DEFAULT NULL,
+  `city` varchar(32) DEFAULT NULL,
+  `district` varchar(10) DEFAULT NULL,
+  `dispatch_date_time` datetime DEFAULT NULL,
+  `incident_type` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`event_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const get = require('get');
 
 const ProcessPDF = require('./lib/process-pdf');
+const DB = require('./lib/db.js');
 
 const pdfGet = get('http://oldweb.rockfordil.gov/PDReports/72-Hour%20CFS%20Log.pdf');
+
 const processor = new ProcessPDF();
+const db = new DB();
 
 pdfGet.asBuffer((err, data) => processor.load(data));
 
 processor.completed.then((dispatchEvents) => {
-	console.log(processor.updated, dispatchEvents)
-	debugger;
-	// TODO: after paring down the data to only what doesn't exist in the db yet
-	// (pseudocode) db.get(newestRow.date) && dispatchEvents.filter(({ date }) => date > newestRow.date)
-	// TODO: do a db write of the filtered dataset
+	return db.getNewestId().then((id) => {
+		let eid = id[0].eid || '17-0';
+
+		return db.insertEvents(dispatchEvents.filter(({ event_number }) => event_number > eid));
+	});
+}).then((res) => {
+	console.log(res);
+	process.exit();
+}, (err) => {
+	console.log(err);
+	process.exit();
 });
